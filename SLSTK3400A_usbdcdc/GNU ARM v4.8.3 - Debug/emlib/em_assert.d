bf113db109708587b15cf894a33d1f27c483283e
Iliya Iliev <iliyailiev3592@gmail.com>
2019-02-22 08:01:31 +0200

    Add usbdrv transmit functions
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

---
+++ SLSTK3400A_usbdcdc/GNU ARM v4.8.3 - Debug/emlib/em_assert.d
@@ -0,0 +1,4 @@
+emlib/em_assert.o: ../emlib/em_assert.c \
+ /home/iliya/work/SL/SimplicityStudio/SimplicityStudio_v3/developer/sdks/efm32/v2/emlib/inc/em_assert.h
+
+/home/iliya/work/SL/SimplicityStudio/SimplicityStudio_v3/developer/sdks/efm32/v2/emlib/inc/em_assert.h:
